0127086fd42d4bdce25cde941d649aeff6a22d8d
LucasToledo88 <lucastoledo88@gmail.com>
2024-08-31 23:40:02 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
+
+app.use(express.json());
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+
+app.listen(8080, () => {
+  console.log("Servidor escuchando en el puerto 8080");
+});
